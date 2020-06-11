800ee384c96da8d05d82a51368fc024f62cf3d6e
Art Astillero <aastillero@v-admin-ws-13.useradd.com>
2020-06-11 06:22:51 -0400

    ET-90 matthew

--- lib/screens/control_panel.dart
+++ lib/screens/control_panel.dart
@@ -211,7 +211,7 @@ class _control_panelState extends State<control_panel> {
       LoadContent.saveEventConfig(context).then((val) {
         MainFrameLoadingIndicator.hideLoading(context);
         ScreenUtil.showMainFrameDialog(
-            context, "Save Success", "Event Info Saved. Press OK.").then((val) {
+            context, "Save Success", "'Event info saved. Press OK").then((val) {
           Navigator.maybePop(context);
         });
       });
